8c648613a7fe466ca6efcc2758bb5243c8d748cd
baikangwang <baikangwang@hotmail.com>
2017-03-27 23:55:42 +0800

    Implemented the action 'new' and create the action 'create'

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,28 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express' });
 });
+
+// add blog
+router.get("/new", function(req, res, next) {
+    res.render("new");
+});
+
+// blog list view
+
+// remove blog
+
+// update blog
+
+// blog details view
+
+// blog edit view
+
+// save blog
+router.post("/create", function(req, res, next) {
+    res.send(req.body);
+});
+
 
 module.exports = router;
