997feb84a4b92690c2114c16cd0a41527bb78f9f
baikangwang <baikangwang@hotmail.com>
2017-03-27 17:46:31 +0800

    Initial checkin

---
+++ modules/blog/index.js
@@ -0,0 +1,17 @@
+// add blog
+app.get("/new", function(req, res) {
+    res.render("new.ejs");
+
+});
+
+// blog list view
+
+// remove blog
+
+// update blog
+
+// blog details view
+
+// blog edit view
+
+// save blog
